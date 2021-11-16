5fc04977320b04eb996b9cba52c33bed5d242e81
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2021-11-16 10:48:40 -0500

    Uploaded from GitHub Desktop by INAM

---
+++ Front End basic projects/5. Tab/1.Tabs/1. Toggleable Tabs/Javascript.js
@@ -0,0 +1,15 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    // javaScript make sure tabcontent display none through loop for 
+tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // JavaScript want to change class name to active 
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
